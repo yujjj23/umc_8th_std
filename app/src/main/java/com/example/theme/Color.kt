77da139fb67f93b932baee1d0b610a562d3a798c
yujjj23 <bt26aza2018@gmail.com>
2025-05-13 21:19:15 +0900

    week7

--- app/src/main/java/com/example/theme/Color.kt
+++ app/src/main/java/com/example/theme/Color.kt
@@ -1,4 +1,4 @@
-package com.example.timer.ui.theme
+package com.example.test.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
--- app/src/main/java/com/example/theme/Theme.kt
+++ app/src/main/java/com/example/theme/Theme.kt
@@ -1,4 +1,4 @@
-package com.example.timer.ui.theme
+package com.example.test.ui.theme
 
 import android.app.Activity
 import android.os.Build
@@ -34,7 +34,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun TimerTheme(
+fun TestTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
